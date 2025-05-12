6f64a94087911a100fc244efbeb046fe8da86754
yyubin <hazing120@gmail.com>
2025-05-13 02:44:50 +0900

    chore: first commit

---
+++ src/main/java/org/ekujo/gesellschaft/GesellschaftApplication.java
@@ -0,0 +1,13 @@
+package org.ekujo.gesellschaft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesellschaftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesellschaftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ekujo/gesellschaft/GesellschaftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ekujo.gesellschaft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesellschaftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

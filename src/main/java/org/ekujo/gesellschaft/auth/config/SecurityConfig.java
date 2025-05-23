9459a9ffae9f783f5b5347de3567d7d47bd1ef17
yyubin <hazing120@gmail.com>
2025-05-23 16:12:20 +0900

    chore: add cors origins

--- src/main/java/org/ekujo/gesellschaft/auth/config/SecurityConfig.java
+++ src/main/java/org/ekujo/gesellschaft/auth/config/SecurityConfig.java
@@ -40,7 +40,11 @@ public class SecurityConfig {
                 .cors(cors -> cors
                         .configurationSource(request -> {
                             CorsConfiguration cfg = new CorsConfiguration();
-                            cfg.setAllowedOrigins(List.of("http://localhost:5173"));
+                            cfg.setAllowedOrigins(List.of(
+                                    "http://localhost:5173",
+                                    "https://admin.gesellschaft.site",
+                                    "https://gesellschaft.site"
+                            ));
                             cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
                             cfg.setAllowedHeaders(List.of("*"));
                             cfg.setAllowCredentials(true);
